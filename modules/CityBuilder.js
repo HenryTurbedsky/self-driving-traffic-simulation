cee7668cd18015644f0d0be99ddc09386976c67e
TurboHenry <turbo.henryg@gmail.com>
2022-10-29 16:24:35 -0700

    added SelectionButton

--- modules/CityBuilder.js
+++ modules/CityBuilder.js
@@ -22,10 +22,11 @@ export class CityBuilder {
             }
 
             if(event.button == 1){
-                if(this.selection){
-                    if(this.selection.objectType == "Intersection"){
-
-                        //return;
+                if(this.selection?.active){
+                    if(this.selection.checkIfClicked(event.mouse.x, event.mouse.y)){
+                        return true;
+                    }else{
+                        console.log("Selected Empty Space");
                     }
                 }
 
@@ -46,13 +47,16 @@ export class CityBuilder {
 
 
 class Selection {
-
     selectedObj = null;
     objectType = null;
     active = false;
 
     x = 0;
     y = 0;
+    width = 0;
+    height = 0;
+
+    buttons = [];
 
     constructor(object){
         this.selectedObj = object;
@@ -64,32 +68,85 @@ class Selection {
 
         this.x = object.x;
         this.y = object.y;
+
+        if(this.objectType == "Intersection"){
+            this.width = Intersection.width;
+            this.height = Intersection.height;
+
+            const x = this.x;
+            const y = this.y;
+            
+            if(!object.roads[0])
+                this.buttons.push(new SelectionButton(x+this.width/4, y, this.width/2, this.height/4));
+            if(!object.roads[1])
+                this.buttons.push(new SelectionButton(x+this.width*3/4, y+this.height/4, this.width/4, this.height/2));
+            if(!object.roads[2])
+                this.buttons.push(new SelectionButton(x+this.width/4, y+this.height*3/4, this.width/2, this.height/4));
+            if(!object.roads[3])
+                this.buttons.push(new SelectionButton(x, y+this.height/4, this.width/4, this.height/2));
+        }
     }
 
-    deselect(){
-        this.active = false;
+    checkIfClicked(x, y){
+        if(Math.abs((this.x + this.width/2) - x) < this.width/2 && Math.abs((this.y + this.height/2) - y) < this.height/2) {
+            for(let i = 0; i < this.buttons.length; i++){
+                let isClicked = this.buttons[i].checkIfClicked(x, y);
+                if(isClicked) return isClicked;
+            }
+            return this.clicked();
+        }
+        return false;
     }
-    select(){
-        this.active = true;
+    clicked(){
+        console.log("Selection was clicked");
+        return true;
     }
 
     draw(ctx){
-        if(this.objectType == "Intersection"){
-            const x = this.x;
-            const y = this.y;
+        const x = this.x;
+        const y = this.y;
 
-            ctx.fillStyle = "rgba(0, 0, 255, 0.3)";
-            ctx.fillRect(x, y, Intersection.width, Intersection.height);
-            
+        if(this.active) ctx.fillStyle = "rgba(0, 0, 255, 0.3)";
+        else ctx.fillStyle = "rgba(255, 0, 0, 0.3)"
 
-            ctx.fillStyle = "rgba(0, 255, 0, 1)";
-            ctx.fillRect(x+Intersection.width/4, y, Intersection.width/2, Intersection.height/4);
-            
-            ctx.fillRect(x+Intersection.width*3/4, y+Intersection.height/4, Intersection.width/4, Intersection.height/2);
-            
-            ctx.fillRect(x+Intersection.width/4, y+Intersection.height*3/4, Intersection.width/2, Intersection.height/4);
-            
-            ctx.fillRect(x, y+Intersection.height/4, Intersection.width/4, Intersection.height/2);
-        }   
+        ctx.fillRect(x, y, this.width, this.height);
+
+        this.buttons.forEach(button => {
+            button.draw(ctx);
+        });
+    }
+}
+
+
+class SelectionButton {
+    x = 0;
+    y = 0;
+    width = 0;
+    height = 0;
+    color = "";
+
+    constructor(x, y, width, height, color="rgba(0, 255, 0, 1)"){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.color = color;
+    }
+
+    checkIfClicked(x, y){
+        if(Math.abs((this.x + this.width/2) - x) < this.width/2 && Math.abs((this.y + this.height/2) - y) < this.height/2) {
+            return this.clicked();
+        }
+        return false;
+    }
+    clicked(){
+        console.log("Button was clicked");
+        return true;
+    }
+
+    draw(ctx){
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
